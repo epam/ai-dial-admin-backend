5cf589b1ed72245bea2713ba3269dea83c462ced
Kiryl Kurnosenka <kiryl_kurnosenka@epam.com>
2025-07-12 16:07:41 +0300

    fix: resolve adapter from adapters with same base endpoint (#50)

--- src/main/java/com/epam/aidial/cfg/dao/jpa/AdapterJpaRepository.java
+++ src/main/java/com/epam/aidial/cfg/dao/jpa/AdapterJpaRepository.java
@@ -7,11 +7,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface AdapterJpaRepository extends CrudRepository<AdapterEntity, String> {
 
-    Optional<AdapterEntity> findByBaseEndpoint(String endpoint);
+    Iterable<AdapterEntity> findByBaseEndpointOrderByNameAsc(String endpoint);
 
     @Query("DELETE FROM AdapterEntity a WHERE a.name NOT IN :names")
     @Modifying
