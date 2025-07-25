c1687c54244a31d9c6dfb100d96c9b14230301d2
Vladyslav Pasichniuk <vladyslav_pasichniuk@epam.com>
2025-07-25 11:15:42 +0300

    Add EndpointsRefreshEnabledCondition

---
+++ src/main/java/com/epam/aidial/cfg/domain/service/EndpointsRefreshEnabledCondition.java
@@ -0,0 +1,16 @@
+package com.epam.aidial.cfg.domain.service;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+public class EndpointsRefreshEnabledCondition implements Condition {
+
+    @Override
+    public boolean matches(ConditionContext context, @NotNull AnnotatedTypeMetadata metadata) {
+        final String refreshEnabled = context.getEnvironment()
+                .getProperty("external.deployment.endpoint.refresh.enabled", "false");
+        return Boolean.parseBoolean(refreshEnabled);
+    }
+}
--- src/main/java/com/epam/aidial/cfg/domain/service/EndpointsRefresherScheduledService.java
+++ src/main/java/com/epam/aidial/cfg/domain/service/EndpointsRefresherScheduledService.java
@@ -3,7 +3,7 @@ package com.epam.aidial.cfg.domain.service;
 import com.epam.aidial.cfg.configuration.logging.LogExecution;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -11,21 +11,14 @@ import org.springframework.stereotype.Service;
 @Service
 @LogExecution
 @RequiredArgsConstructor
+@Conditional(EndpointsRefreshEnabledCondition.class)
 public class EndpointsRefresherScheduledService {
 
     private final InterceptorService interceptorService;
 
-    @Value("${external.deployment.endpoint.refresh.enabled}")
-    private boolean enableEndpointsRefresh;
-
     // TODO [VPA]: use system user
     @Scheduled(fixedDelayString = "${external.deployment.endpoint.refresh.interval}")
     public void refreshEndpoints() {
-        if (!enableEndpointsRefresh) {
-            log.debug("External deployment endpoints refresh is disabled");
-            return;
-        }
-
         try {
             log.debug("Refreshing interceptor endpoints where source is container");
             interceptorService.refreshEndpoints();
