5511b86277c49d844a9307001497a4c1b74fad9b
Vladyslav Pasichniuk <vladyslav_pasichniuk@epam.com>
2025-07-31 11:05:56 +0300

    Add @Audited to TimeTrackableEntity

--- src/main/java/com/epam/aidial/cfg/dao/model/TimeTrackableEntity.java
+++ src/main/java/com/epam/aidial/cfg/dao/model/TimeTrackableEntity.java
@@ -5,12 +5,14 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.envers.Audited;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @Setter
+@Audited
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class TimeTrackableEntity<ID> extends AbstractEntity<ID> {
