d6853c089fc56a9581fbd369c267a42d108c6d25
Vladyslav Pasichniuk <vladyslav_pasichniuk@epam.com>
2025-08-11 14:34:17 +0300

    Fix MS SQL migration 1.50

--- src/main/resources/db/migration/MS_SQL_SERVER/V1.50__AddNewFieldsToAppTypeSchemaEntityTable.sql
+++ src/main/resources/db/migration/MS_SQL_SERVER/V1.50__AddNewFieldsToAppTypeSchemaEntityTable.sql
@@ -1,7 +1,7 @@
 -- add new fields to application_type_schema_entity table
-alter table if exists application_type_schema_entity add column if not exists title text;
-alter table if exists application_type_schema_entity add column if not exists type varchar(64);
+alter table application_type_schema_entity add title text;
+alter table application_type_schema_entity add type varchar(64);
 
 -- add new fields to application_type_schema_entity_aud table
-alter table if exists application_type_schema_entity_aud add column if not exists title text;
-alter table if exists application_type_schema_entity_aud add column if not exists type varchar(64);
+alter table application_type_schema_entity_aud add title text;
+alter table application_type_schema_entity_aud add type varchar(64);
