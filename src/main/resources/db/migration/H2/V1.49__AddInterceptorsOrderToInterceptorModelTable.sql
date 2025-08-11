a8e2b36ecaf1424f8a5da42cf4613bdcd889e752
Kiryl_Kurnosenka <Kiryl_Kurnosenka@epam.com>
2025-08-11 13:45:48 +0300

    fix: combine migrations

--- src/main/resources/db/migration/H2/V1.49__AddInterceptorsOrderToInterceptorModelTable.sql
+++ src/main/resources/db/migration/H2/V1.49__AddInterceptorsOrderToInterceptorModelTable.sql
@@ -1,2 +1,13 @@
 alter table if exists interceptor_model add column if not exists interceptors_order integer;
 alter table if exists interceptor_model_aud add column if not exists interceptors_order integer;
+
+update interceptor_model set interceptors_order = 0;
+update interceptor_model_aud set interceptors_order = 0;
+
+alter table interceptor_model alter column interceptors_order set not null;
+alter table interceptor_model_aud alter column interceptors_order set not null;
+
+alter table interceptor_model_aud drop primary key;
+
+alter table interceptor_model add primary key (model_name, interceptor_name, interceptors_order);
+alter table interceptor_model_aud add primary key (model_name, rev, interceptor_name, interceptors_order);
