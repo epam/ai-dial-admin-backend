f04417f38e4da3106963c8739e751f8826656af9
Kiryl_Kurnosenka <Kiryl_Kurnosenka@epam.com>
2025-08-14 19:53:44 +0300

    fix: adjust comment

--- src/main/java/com/epam/aidial/cfg/service/transfer/importer/RoleBasedImporter.java
+++ src/main/java/com/epam/aidial/cfg/service/transfer/importer/RoleBasedImporter.java
@@ -14,11 +14,11 @@ public abstract class RoleBasedImporter {
                                  Map<String, Role> roles,
                                  Deployment newDeployment,
                                  boolean isPreview) {
-        // For the deployment we need to leave only those role limits which roles are present in config. It means that:
-        // 1. If role limit already exists - we save its actual state here during deployment update and then during
+        // For the deployment we need to leave only those role limits which role is present in config. It means that:
+        // 1. If role limit already exists - we save its new state here during deployment update and then during
         //    role update. If we try to remove it here and re-add it later during role import, we get
         //    org.hibernate.ObjectDeletedException: deleted instance passed to merge.
-        // 2. If role limit doesn't exist - we just show its state on preview, but save it later during role import.
+        // 2. If role limit doesn't exist - we just show its new state on preview, but save it later during role import.
         //    If we try to save it here and during role import, we get
         //    org.hibernate.NonUniqueObjectException: A different object with the same identifier value was already
         //    associated with the session.
