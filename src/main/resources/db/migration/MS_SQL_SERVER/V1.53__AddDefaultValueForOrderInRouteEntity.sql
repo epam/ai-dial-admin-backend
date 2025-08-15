46ef5f5c20d3013963b2ffc5ad752111f7d59b83
Vladyslav Pasichniuk <vladyslav_pasichniuk@epam.com>
2025-08-15 16:50:35 +0300

    fix: rename order default value constraint for aud table

--- src/main/resources/db/migration/MS_SQL_SERVER/V1.53__AddDefaultValueForOrderInRouteEntity.sql
+++ src/main/resources/db/migration/MS_SQL_SERVER/V1.53__AddDefaultValueForOrderInRouteEntity.sql
@@ -5,4 +5,4 @@ ALTER TABLE route_entity ALTER COLUMN order_value INT NOT NULL;
 
 -- Update route_entity_aud
 UPDATE route_entity_aud SET order_value = 2147483647 WHERE order_value IS NULL;
-ALTER TABLE route_entity_aud ADD CONSTRAINT df_order_value DEFAULT 2147483647 FOR order_value;
+ALTER TABLE route_entity_aud ADD CONSTRAINT df_order_value_aud DEFAULT 2147483647 FOR order_value;
