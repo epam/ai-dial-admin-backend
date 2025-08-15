3ebffdbac1a8125ffcb75e4af4a7094185e78854
Vladyslav Pasichniuk <vladyslav_pasichniuk@epam.com>
2025-08-15 16:20:24 +0300

    fix: add not null constraint for 'order' in route

--- src/main/resources/db/migration/MS_SQL_SERVER/V1.53__AddDefaultValueForOrderInRouteEntity.sql
+++ src/main/resources/db/migration/MS_SQL_SERVER/V1.53__AddDefaultValueForOrderInRouteEntity.sql
@@ -1,5 +1,9 @@
+-- Update route_entity
 UPDATE route_entity SET order_value = 2147483647 WHERE order_value IS NULL;
-UPDATE route_entity_aud SET order_value = 2147483647 WHERE order_value IS NULL;
-
 ALTER TABLE route_entity ADD CONSTRAINT df_order_value DEFAULT 2147483647 FOR order_value;
+ALTER TABLE route_entity ALTER COLUMN order_value INT NOT NULL;
+
+-- Update route_entity_aud
+UPDATE route_entity_aud SET order_value = 2147483647 WHERE order_value IS NULL;
 ALTER TABLE route_entity_aud ADD CONSTRAINT df_order_value DEFAULT 2147483647 FOR order_value;
+ALTER TABLE route_entity_aud ALTER COLUMN order_value INT NOT NULL;
