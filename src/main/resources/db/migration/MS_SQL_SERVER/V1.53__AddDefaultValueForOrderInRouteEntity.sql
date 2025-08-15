870a894b582ccca6abe0e5afb21623cb832a4828
Vladyslav Pasichniuk <vladyslav_pasichniuk@epam.com>
2025-08-15 16:57:02 +0300

    fix: set order in existing records in route_entity_aud if action is not deletion

--- src/main/resources/db/migration/MS_SQL_SERVER/V1.53__AddDefaultValueForOrderInRouteEntity.sql
+++ src/main/resources/db/migration/MS_SQL_SERVER/V1.53__AddDefaultValueForOrderInRouteEntity.sql
@@ -4,5 +4,5 @@ ALTER TABLE route_entity ADD CONSTRAINT df_order_value DEFAULT 2147483647 FOR or
 ALTER TABLE route_entity ALTER COLUMN order_value INT NOT NULL;
 
 -- Update route_entity_aud
-UPDATE route_entity_aud SET order_value = 2147483647 WHERE order_value IS NULL;
+UPDATE route_entity_aud SET order_value = 2147483647 WHERE order_value IS NULL AND revtype != 2;
 ALTER TABLE route_entity_aud ADD CONSTRAINT df_order_value_aud DEFAULT 2147483647 FOR order_value;
--- src/main/resources/db/migration/POSTGRES/V1.53__AddDefaultValueForOrderInRouteEntity.sql
+++ src/main/resources/db/migration/POSTGRES/V1.53__AddDefaultValueForOrderInRouteEntity.sql
@@ -4,5 +4,5 @@ ALTER TABLE route_entity ALTER COLUMN order_value SET DEFAULT 2147483647;
 ALTER TABLE route_entity ALTER COLUMN order_value SET NOT NULL;
 
 -- Update route_entity_aud
-UPDATE route_entity_aud SET order_value = 2147483647 WHERE order_value IS NULL;
+UPDATE route_entity_aud SET order_value = 2147483647 WHERE order_value IS NULL AND revtype != 2;
 ALTER TABLE route_entity_aud ALTER COLUMN order_value SET DEFAULT 2147483647;
