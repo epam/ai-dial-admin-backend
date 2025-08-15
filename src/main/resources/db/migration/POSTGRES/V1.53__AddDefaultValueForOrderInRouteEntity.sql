523611632455ded126e433b11e4706ca49ad1eed
Vladyslav Pasichniuk <vladyslav_pasichniuk@epam.com>
2025-08-15 16:41:00 +0300

    fix: remove not null constraint for 'order' in route_entity_aud

--- src/main/resources/db/migration/POSTGRES/V1.53__AddDefaultValueForOrderInRouteEntity.sql
+++ src/main/resources/db/migration/POSTGRES/V1.53__AddDefaultValueForOrderInRouteEntity.sql
@@ -6,4 +6,3 @@ ALTER TABLE route_entity ALTER COLUMN order_value SET NOT NULL;
 -- Update route_entity_aud
 UPDATE route_entity_aud SET order_value = 2147483647 WHERE order_value IS NULL;
 ALTER TABLE route_entity_aud ALTER COLUMN order_value SET DEFAULT 2147483647;
-ALTER TABLE route_entity_aud ALTER COLUMN order_value SET NOT NULL;
