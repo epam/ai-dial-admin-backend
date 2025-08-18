ce52dcb2499900dcec3daade0bbae44ef651f9ec
Vladyslav Pasichniuk <vladyslav_pasichniuk@epam.com>
2025-08-18 16:31:43 +0300

    minor fixes

--- src/main/java/com/epam/aidial/core/config/Config.java
+++ src/main/java/com/epam/aidial/core/config/Config.java
@@ -26,7 +26,7 @@ public class Config {
     private Map<String, CoreModel> models = new HashMap<>();
     private Map<String, CoreAddon> addons = new HashMap<>();
     private Map<String, CoreApplication> applications = new HashMap<>();
-    private Map<String, CoreToolSet> toolsets = Map.of();
+    private Map<String, CoreToolSet> toolsets = new HashMap<>();
     private Assistants assistant = new Assistants();
     private Map<String, CoreKey> keys = new HashMap<>();
     private Map<String, CoreRole> roles = new HashMap<>();
