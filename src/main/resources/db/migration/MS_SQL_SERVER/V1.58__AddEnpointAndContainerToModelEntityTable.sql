57b4911b8aaaefcf0ab5093ebcdff515f3bb0aa5
Vladyslav Pasichniuk <vladyslav_pasichniuk@epam.com>
2025-08-29 10:03:05 +0300

    Add migration 1.58

---
+++ src/main/resources/db/migration/MS_SQL_SERVER/V1.58__AddEnpointAndContainerToModelEntityTable.sql
@@ -0,0 +1,13 @@
+-- add new fields to model_entity table
+alter table model_entity add endpoint nvarchar(max);
+alter table model_entity add container_id nvarchar(max);
+alter table model_entity add completion_endpoint_path nvarchar(max);
+alter table model_entity add configuration_endpoint_path nvarchar(max);
+alter table model_entity add adapter_completion_endpoint_path nvarchar(max);
+
+-- add new fields to model_entity_aud table
+alter table model_entity_aud add endpoint nvarchar(max);
+alter table model_entity_aud add container_id nvarchar(max);
+alter table model_entity_aud add completion_endpoint_path nvarchar(max);
+alter table model_entity_aud add configuration_endpoint_path nvarchar(max);
+alter table model_entity_aud add adapter_completion_endpoint_path nvarchar(max);
