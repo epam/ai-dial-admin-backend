8f82370682cccd1208ee745bdca7170162f9c17d
Kiryl_Kurnosenka <Kiryl_Kurnosenka@epam.com>
2025-08-29 23:10:31 +0300

    fix: remove applications with invalid application type schema from exported core config

--- src/test/java/com/epam/aidial/cfg/service/normalizer/impl/ApplicationTypeSchemaCoreConfigNormalizerTest.java
+++ src/test/java/com/epam/aidial/cfg/service/normalizer/impl/ApplicationTypeSchemaCoreConfigNormalizerTest.java
@@ -1,9 +1,11 @@
 package com.epam.aidial.cfg.service.normalizer.impl;
 
 import com.epam.aidial.core.config.Config;
+import com.epam.aidial.core.config.CoreApplication;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -31,64 +33,51 @@ class ApplicationTypeSchemaCoreConfigNormalizerTest {
     }
 
     @Test
-    void testNormalize_applicationTypeSchemaDoesNotConformToMetaSchema_removeApplicationTypeSchema() {
+    void testNormalize_applicationTypeSchemaDoesNotConformToMetaSchema_removeApplicationTypeSchemaAndAssociatedApplications() {
         // given
         String applicationTypeSchema = """
                 {
-                     "properties": {},
-                     "applications": [],
-                     "createdAt": "2025-08-29T11:34:26.709Z",
-                     "updatedAt": "2025-08-29T11:34:26.709Z",
-                     "$schema": "https://dial.epam.com/application_type_schemas/schema#",
-                     "$id": "https://runner-char",
-                     "dial:applicationTypeDisplayName": "char1",
-                     "dial:applicationTypeCompletionEndpoint": "https://app_hostname/openai/deployments/app_name/chat/completions",
-                     "dial:applicationTypeRoutes": [
-                         {
-                             "isPublic": false,
-                             "name": "char_route",
-                             "rewritePath": false,
-                             "paths": [
-                                 "/auto-test-global-route/some/suffix/here",
-                                 ""
-                             ],
-                             "methods": [
-                                 "POST",
-                                 "GET"
-                             ],
-                             "upstreams": [
-                                 {
-                                     "weight": 1,
-                                     "tier": 0
-                                 }
-                             ],
-                             "maxRetryAttempts": 1,
-                             "order": 1,
-                             "permissions": [
-                                 "read",
-                                 "write"
-                             ]
-                         }
-                     ],
-                     "$defs": {}
-                 }
+                    "properties": {},
+                    "applications": [],
+                    "createdAt": "2025-08-29T11:34:26.709Z",
+                    "updatedAt": "2025-08-29T11:34:26.709Z",
+                    "$schema": "https://dial.epam.com/application_type_schemas/schema#",
+                    "$id": "https://runner-char",
+                    "dial:applicationTypeDisplayName": "char1",
+                    "$defs": {}
+                }
                 """;
 
         Map<String, String> applicationTypeSchemasMap = new HashMap<>();
         applicationTypeSchemasMap.put("https://runner-char", applicationTypeSchema);
 
+        CoreApplication application1 = new CoreApplication();
+        application1.setApplicationTypeSchemaId(URI.create("https://runner-char"));
+
+        CoreApplication application2 = new CoreApplication();
+        application2.setApplicationTypeSchemaId(URI.create("https://runner-char-new"));
+
+        Map<String, CoreApplication> applicationsMap = new HashMap<>();
+        applicationsMap.put("testApplication1", application1);
+        applicationsMap.put("testApplication2", application2);
+
         Config config = new Config();
         config.setApplicationTypeSchemas(applicationTypeSchemasMap);
+        config.setApplications(applicationsMap);
 
         // when
         normalizer.normalize(config);
 
         // then
-        assertThat(config.getApplications()).isEmpty();
+        assertThat(config.getApplicationTypeSchemas()).isEmpty();
+        assertThat(config.getApplications()).hasSize(1).satisfies(applications -> {
+            assertThat(applications.get("testApplication1")).isNull();
+            assertThat(applications.get("testApplication2")).isNotNull();
+        });
     }
 
     @Test
-    void testNormalize_applicationTypeSchemaConformsToMetaSchema_doesNotRemoveApplicationTypeSchema() {
+    void testNormalize_applicationTypeSchemaConformsToMetaSchema_doesNotRemoveApplicationTypeSchemaAndAssociatedApplications() {
         // given
         String applicationTypeSchema = """
                 {
@@ -107,8 +96,19 @@ class ApplicationTypeSchemaCoreConfigNormalizerTest {
         Map<String, String> applicationTypeSchemasMap = new HashMap<>();
         applicationTypeSchemasMap.put("https://runner-char", applicationTypeSchema);
 
+        CoreApplication application1 = new CoreApplication();
+        application1.setApplicationTypeSchemaId(URI.create("https://runner-char"));
+
+        CoreApplication application2 = new CoreApplication();
+        application2.setApplicationTypeSchemaId(URI.create("https://runner-char-new"));
+
+        Map<String, CoreApplication> applicationsMap = new HashMap<>();
+        applicationsMap.put("testApplication1", application1);
+        applicationsMap.put("testApplication2", application2);
+
         Config config = new Config();
         config.setApplicationTypeSchemas(applicationTypeSchemasMap);
+        config.setApplications(applicationsMap);
 
         // when
         normalizer.normalize(config);
@@ -117,6 +117,10 @@ class ApplicationTypeSchemaCoreConfigNormalizerTest {
         assertThat(config.getApplicationTypeSchemas()).hasSize(1).satisfies(applicationTypeSchemas -> {
             String schema = applicationTypeSchemas.get("https://runner-char");
             assertThat(schema).isNotNull();
+        });
+        assertThat(config.getApplications()).hasSize(2).satisfies(applications -> {
+            assertThat(applications.get("testApplication1")).isNotNull();
+            assertThat(applications.get("testApplication2")).isNotNull();
         });
     }
 
