b3d00742e14bae2bba8389c780d12498e61f1ed5
Kiryl Kurnosenka <kiryl_kurnosenka@epam.com>
2025-09-03 14:23:00 +0300

    fix: set `parallelToolCallsSupported` to true if null in database (#222)

---
+++ src/main/resources/db/migration/MS_SQL_SERVER/V1.58__UpdateParallelToolCallsSupportedToTrueIfNull.sql
@@ -0,0 +1,8 @@
+update model_entity set parallel_tool_calls_supported = 1 where parallel_tool_calls_supported is null;
+update model_entity_aud set parallel_tool_calls_supported = 1 where parallel_tool_calls_supported is null and revtype != 2;
+
+update application_entity set parallel_tool_calls_supported = 1 where parallel_tool_calls_supported is null;
+update application_entity_aud set parallel_tool_calls_supported = 1 where parallel_tool_calls_supported is null and revtype != 2;
+
+update assistants_property_entity set parallel_tool_calls_supported = 1 where parallel_tool_calls_supported is null;
+update assistants_property_entity_aud set parallel_tool_calls_supported = 1 where parallel_tool_calls_supported is null and revtype != 2;
