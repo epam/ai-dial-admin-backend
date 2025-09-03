1ddecea1746b80953e0ce10ba710cdd528a8a6d3
Vladyslav Pasichniuk <vladyslav_pasichniuk@epam.com>
2025-09-03 16:48:04 +0300

    Update migration that adds endpoint paths & containerId to model tables

--- src/main/resources/db/migration/POSTGRES/V1.59__AddEndpointPathsAndContainerIdToModelEntityTable.sql
+++ src/main/resources/db/migration/POSTGRES/V1.59__AddEndpointPathsAndContainerIdToModelEntityTable.sql
@@ -1,12 +1,10 @@
 -- add new fields to model_entity table
-alter table if exists model_entity add column if not exists endpoint text;
 alter table if exists model_entity add column if not exists container_id text;
 alter table if exists model_entity add column if not exists completion_endpoint_path text;
 alter table if exists model_entity add column if not exists configuration_endpoint_path text;
 alter table if exists model_entity add column if not exists adapter_completion_endpoint_path text;
 
 -- add new fields to model_entity_aud table
-alter table if exists model_entity_aud add column if not exists endpoint text;
 alter table if exists model_entity_aud add column if not exists container_id text;
 alter table if exists model_entity_aud add column if not exists completion_endpoint_path text;
 alter table if exists model_entity_aud add column if not exists configuration_endpoint_path text;
