f05c6ff8b410e1d63a7921315a424ac2aec5f385
Vladyslav Pasichniuk <vladyslav_pasichniuk@epam.com>
2025-09-09 17:29:47 +0300

    hotfix: populate adapter_completion_endpoint

---
+++ src/main/resources/db/migration/POSTGRES/V1.60__PopulateAdapterCompletionEndpointPathInModelEntity.sql
@@ -0,0 +1,19 @@
+-- Update model_entity
+UPDATE model_entity
+SET adapter_completion_endpoint_path =
+    COALESCE(endpoint_deployment_name, '') ||
+    CASE
+        WHEN type = 0 THEN '/chat/completions'
+        ELSE '/embeddings'
+    END
+WHERE adapter_name IS NOT NULL;
+
+-- Update model_entity_aud
+UPDATE model_entity_aud
+SET adapter_completion_endpoint_path =
+    COALESCE(endpoint_deployment_name, '') ||
+    CASE
+        WHEN type = 0 THEN '/chat/completions'
+        ELSE '/embeddings'
+    END
+WHERE adapter_name IS NOT NULL;
