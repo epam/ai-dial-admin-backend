16147ef839d190ba8dfc2f81637dd2d1d533d8fc
Vladyslav Pasichniuk <vladyslav_pasichniuk@epam.com>
2025-09-17 18:02:41 +0300

    Fix MS SQL migration 1.65

--- src/main/resources/db/migration/MS_SQL_SERVER/V1.65__AddCostLimitFieldsToRoleTable.sql
+++ src/main/resources/db/migration/MS_SQL_SERVER/V1.65__AddCostLimitFieldsToRoleTable.sql
@@ -1,25 +1,38 @@
 -- add cost limit's fields to role_entity
-alter table role_entity add cost_limit_minute numeric;
+alter table role_entity add cost_limit_minute numeric(20,0);
 alter table role_entity add constraint df_role_entity_cost_limit_minute default 9223372036854775807 for cost_limit_minute;
-alter table role_entity alter column cost_limit_minute numeric not null;
+go
+update role_entity set cost_limit_minute = 9223372036854775807 where cost_limit_minute is null;
+go
+alter table role_entity alter column cost_limit_minute numeric(20,0) not null;
 
-alter table role_entity add cost_limit_day numeric;
+alter table role_entity add cost_limit_day numeric(20,0);
 alter table role_entity add constraint df_role_entity_cost_limit_day default 9223372036854775807 for cost_limit_day;
-alter table role_entity alter column cost_limit_day numeric not null;
+go
+update role_entity set cost_limit_day = 9223372036854775807 where cost_limit_day is null;
+go
+alter table role_entity alter column cost_limit_day numeric(20,0) not null;
 
-alter table role_entity add cost_limit_week numeric;
+alter table role_entity add cost_limit_week numeric(20,0);
 alter table role_entity add constraint df_role_entity_cost_limit_week default 9223372036854775807 for cost_limit_week;
-alter table role_entity alter column cost_limit_week numeric not null;
+go
+update role_entity set cost_limit_week = 9223372036854775807 where cost_limit_week is null;
+go
+alter table role_entity alter column cost_limit_week numeric(20,0) not null;
 
-alter table role_entity add cost_limit_month numeric;
+alter table role_entity add cost_limit_month numeric(20,0);
 alter table role_entity add constraint df_role_entity_cost_limit_month default 9223372036854775807 for cost_limit_month;
-alter table role_entity alter column cost_limit_month numeric not null;
+go
+update role_entity set cost_limit_month = 9223372036854775807 where cost_limit_month is null;
+go
+alter table role_entity alter column cost_limit_month numeric(20,0) not null;
 
 -- add cost limit's fields to role_entity_aud
-alter table role_entity_aud add cost_limit_minute numeric;
-alter table role_entity_aud add cost_limit_day numeric;
-alter table role_entity_aud add cost_limit_week numeric;
-alter table role_entity_aud add cost_limit_month numeric;
+alter table role_entity_aud add cost_limit_minute numeric(20,0);
+alter table role_entity_aud add cost_limit_day numeric(20,0);
+alter table role_entity_aud add cost_limit_week numeric(20,0);
+alter table role_entity_aud add cost_limit_month numeric(20,0);
+go
 
 -- populate existing records with default value (except removals)
 update role_entity_aud set cost_limit_minute = 9223372036854775807 where cost_limit_minute is null and revtype != 2;
