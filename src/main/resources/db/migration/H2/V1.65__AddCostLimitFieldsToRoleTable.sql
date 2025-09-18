f6be4e09a73b7ab4981a05827810adfa0f50dd61
Vladyslav Pasichniuk <vladyslav_pasichniuk@epam.com>
2025-09-18 12:41:09 +0300

    Increase amount of digits before decimal for numeric columns

--- src/main/resources/db/migration/H2/V1.65__AddCostLimitFieldsToRoleTable.sql
+++ src/main/resources/db/migration/H2/V1.65__AddCostLimitFieldsToRoleTable.sql
@@ -1,14 +1,14 @@
 -- add cost limit's fields to role_entity
-alter table if exists role_entity add column if not exists cost_limit_minute numeric default 9223372036854775807 not null;
-alter table if exists role_entity add column if not exists cost_limit_day numeric default 9223372036854775807 not null;
-alter table if exists role_entity add column if not exists cost_limit_week numeric default 9223372036854775807 not null;
-alter table if exists role_entity add column if not exists cost_limit_month numeric default 9223372036854775807 not null;
+alter table if exists role_entity add column if not exists cost_limit_minute numeric(38, 18) default 9223372036854775807 not null;
+alter table if exists role_entity add column if not exists cost_limit_day numeric(38, 18) default 9223372036854775807 not null;
+alter table if exists role_entity add column if not exists cost_limit_week numeric(38, 18) default 9223372036854775807 not null;
+alter table if exists role_entity add column if not exists cost_limit_month numeric(38, 18) default 9223372036854775807 not null;
 
 -- add cost limit's fields to role_entity_aud
-alter table if exists role_entity_aud add column if not exists cost_limit_minute numeric;
-alter table if exists role_entity_aud add column if not exists cost_limit_day numeric;
-alter table if exists role_entity_aud add column if not exists cost_limit_week numeric;
-alter table if exists role_entity_aud add column if not exists cost_limit_month numeric;
+alter table if exists role_entity_aud add column if not exists cost_limit_minute numeric(38, 18);
+alter table if exists role_entity_aud add column if not exists cost_limit_day numeric(38, 18);
+alter table if exists role_entity_aud add column if not exists cost_limit_week numeric(38, 18);
+alter table if exists role_entity_aud add column if not exists cost_limit_month numeric(38, 18);
 
 -- populate existing records with default value (except removals)
 update role_entity_aud set cost_limit_minute = 9223372036854775807 where cost_limit_minute is null and revtype != 2;
