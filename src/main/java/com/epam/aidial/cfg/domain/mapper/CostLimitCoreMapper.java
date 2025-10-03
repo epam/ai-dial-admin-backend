a80de378919faaa068832f3ca6080e3760cff796
Kiryl_Kurnosenka <Kiryl_Kurnosenka@epam.com>
2025-10-03 16:25:34 +0300

    fix: adjust import/export of core role limits (#300)

--- src/main/java/com/epam/aidial/cfg/domain/mapper/CostLimitCoreMapper.java
+++ src/main/java/com/epam/aidial/cfg/domain/mapper/CostLimitCoreMapper.java
@@ -2,12 +2,15 @@ package com.epam.aidial.cfg.domain.mapper;
 
 import com.epam.aidial.cfg.domain.model.CostLimit;
 import com.epam.aidial.core.config.CoreCostLimit;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.NullValueMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface CostLimitCoreMapper {
 
     CoreCostLimit toCoreCostLimit(CostLimit costLimit);
 
+    @BeanMapping(nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
     CostLimit toCostLimit(CoreCostLimit coreCostLimit);
 }
