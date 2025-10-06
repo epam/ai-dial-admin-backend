9a40ae5ec8228ed27c4f20d49127be308a758454
Katerina_Charakhovich <katerinacharakhovich@gmail.com>
2025-10-06 22:03:33 +0200

    check MsSqlServer tests

--- src/main/resources/db/migration/MS_SQL_SERVER/V1.67__SetDisplayNameValueIfNull.sql
+++ src/main/resources/db/migration/MS_SQL_SERVER/V1.67__SetDisplayNameValueIfNull.sql
@@ -14,13 +14,15 @@ alter table addon_entity alter column display_name nvarchar(max) not null;
 update application_entity set display_name = deployment_name where display_name is null or trim(display_name) = '';
 update application_entity_aud set display_name = deployment_name where (display_name is null or trim(display_name) = '') and revtype != 2;
 --  Application tables. Set the column as not null after all nulls are filled
+drop index UX_APPLICATION_ENTITY_DISPLAY_NAME_DISPLAY_VERSION_NOT_NULL on application_entity;
 alter table application_entity alter column display_name nvarchar(255) not null;
+create unique nonclustered index UX_APPLICATION_ENTITY_DISPLAY_NAME_DISPLAY_VERSION_NOT_NULL on application_entity (display_name, display_version) where display_version is not null;
 
 --  Application type schema tables. Fill the display_name with values from the old 'schema_id' column
-update application_type_schema_entity set application_type_display_name = schema_id where application_type_display_name is null;
-update application_type_schema_entity_aud set application_type_display_name = schema_id where application_type_display_name is null and revtype != 2;
+update application_type_schema_entity set application_type_display_name = schema_id where application_type_display_name is null or trim(application_type_display_name) = '';
+update application_type_schema_entity_aud set application_type_display_name = schema_id where (application_type_display_name is null or trim(application_type_display_name) = '') and revtype != 2;
 --  Application type schema tables. Set the column as not null after all nulls are filled
-alter table application_type_schema_entity alter column application_type_display_name set not null;
+alter table application_type_schema_entity alter column application_type_display_name nvarchar(max) not null;
 
 --  Assistant tables. Fill the display_name with values from the old 'deployment_name' column
 update assistant_entity set display_name = deployment_name where display_name is null or trim(display_name) = '';
@@ -50,7 +52,9 @@ alter table key_entity alter column display_name nvarchar(max) not null;
 update model_entity set display_name = deployment_name where display_name is null or trim(display_name) = '';
 update model_entity_aud set display_name = deployment_name where (display_name is null or trim(display_name) = '') and revtype != 2;
 --  Model tables. Set the column as not null after all nulls are filled
+drop index UX_MODEL_ENTITY_DISPLAY_NAME_DISPLAY_VERSION_NOT_NULL on model_entity;
 alter table model_entity alter column display_name nvarchar(255) not null;
+create unique nonclustered index UX_MODEL_ENTITY_DISPLAY_NAME_DISPLAY_VERSION_NOT_NULL on model_entity (display_name, display_version) where display_version is not null;
 
 --  Role tables. Fill the display_name with values from the old 'name' column
 update role_entity set display_name = name where display_name is null or trim(display_name) = '';
