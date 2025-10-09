265a23af0ceb1a0eca8e7782fd985aa95c1dada8
Vladyslav Pasichniuk <vladyslav_pasichniuk@epam.com>
2025-10-09 10:29:53 +0300

    Remove redundant annotation from ContainerEntity

--- src/main/java/com/epam/aidial/cfg/dao/model/ContainerEntity.java
+++ src/main/java/com/epam/aidial/cfg/dao/model/ContainerEntity.java
@@ -1,15 +1,12 @@
 package com.epam.aidial.cfg.dao.model;
 
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
 import org.hibernate.envers.Audited;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Data
 @Audited
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 public abstract class ContainerEntity {
     private String containerId;
     private String containerName;
