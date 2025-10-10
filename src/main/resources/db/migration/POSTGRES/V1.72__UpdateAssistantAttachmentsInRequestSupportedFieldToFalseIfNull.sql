a2a236bf480f748b50fb2e62b228afb5dde5fc8f
Katerina_Charakhovich <katerinacharakhovich@gmail.com>
2025-10-10 12:22:09 +0200

    addressed comments

---
+++ src/main/resources/db/migration/POSTGRES/V1.72__UpdateAssistantAttachmentsInRequestSupportedFieldToFalseIfNull.sql
@@ -0,0 +1,23 @@
+-- set value for AssistantAttachmentsInRequestSupported field for old rows to application_entity table
+update application_entity set assistant_attachments_in_request_supported = false where assistant_attachments_in_request_supported is null;
+
+-- set value for AssistantAttachmentsInRequestSupported field for old rows to application_entity_aud table
+update application_entity_aud set assistant_attachments_in_request_supported = false where assistant_attachments_in_request_supported is null and revtype != 2;
+
+-- set value for AssistantAttachmentsInRequestSupported field for old rows to model_entity table
+update model_entity set assistant_attachments_in_request_supported = false where assistant_attachments_in_request_supported is null;
+
+-- set value for AssistantAttachmentsInRequestSupported field for old rows to model_entity_aud table
+update model_entity_aud set assistant_attachments_in_request_supported = false where assistant_attachments_in_request_supported is null and revtype != 2;
+
+-- set value for AssistantAttachmentsInRequestSupported field for old rows to assistants_property_entity table;
+update assistants_property_entity set assistant_attachments_in_request_supported = false where assistant_attachments_in_request_supported is null;
+
+-- set value for AssistantAttachmentsInRequestSupported field for old rows assistants_property_entity_aud table
+update assistants_property_entity_aud set assistant_attachments_in_request_supported = false where assistant_attachments_in_request_supported is null and revtype != 2;
+
+-- set value for AssistantAttachmentsInRequestSupported field for old rows to interceptor_entity table
+update interceptor_entity set assistant_attachments_in_request_supported = false where assistant_attachments_in_request_supported is null;
+
+-- set value for AssistantAttachmentsInRequestSupported field for old rows to interceptor_entity_aud table
+update interceptor_entity_aud set assistant_attachments_in_request_supported = false where assistant_attachments_in_request_supported is null and revtype != 2;
