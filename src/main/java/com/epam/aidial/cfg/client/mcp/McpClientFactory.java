068684bde90dd233c489ddf19e9e6d56e24f6c70
Siarhei Fedziukovich <Siarhei_Fedziukovich@epam.com>
2025-10-15 00:01:47 +0300

    fix: fix MCP introspection. mcpEndpoint should be passed to builder by parts (baseUri+relativePath)

--- src/main/java/com/epam/aidial/cfg/client/mcp/McpClientFactory.java
+++ src/main/java/com/epam/aidial/cfg/client/mcp/McpClientFactory.java
@@ -7,18 +7,40 @@ import io.modelcontextprotocol.client.McpSyncClient;
 import io.modelcontextprotocol.client.transport.HttpClientSseClientTransport;
 import io.modelcontextprotocol.client.transport.HttpClientStreamableHttpTransport;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
+
+import java.net.URI;
 
 @Component
 @LogExecution
 @RequiredArgsConstructor
 public class McpClientFactory {
 
-    public McpSyncClient create(String baseUrl, Transport transport) {
+    @SneakyThrows
+    public McpSyncClient create(String mcpEndpoint, Transport transport) {
+        var uri = new URI(mcpEndpoint);
+        var baseUrl = uri.getScheme() + "://" + uri.getAuthority();
+        var relativePath = uri.getPath();
+
         var clientTransport = switch (transport) {
-            case HTTP -> HttpClientStreamableHttpTransport.builder(baseUrl).build();
-            case SSE -> HttpClientSseClientTransport.builder(baseUrl).build();
+            case HTTP -> {
+                var builder = HttpClientStreamableHttpTransport.builder(baseUrl);
+                if (StringUtils.isNotBlank(relativePath)) {
+                    builder = builder.endpoint(relativePath);
+                }
+                yield builder.build();
+            }
+            case SSE -> {
+                var builder = HttpClientSseClientTransport.builder(baseUrl);
+                if (StringUtils.isNotBlank(relativePath)) {
+                    builder = builder.sseEndpoint(relativePath);
+                }
+                yield builder.build();
+            }
         };
+
         return McpClient.sync(clientTransport)
                 .build();
     }
