c38ab1d68df3e61e266de5a119292f12f55a861f
Siarhei Fedziukovich <Siarhei_Fedziukovich@epam.com>
2025-10-22 11:53:39 +0300

    fix: remove redundant tests from TimeMapperTest (resolve discussion)

--- src/test/java/com/epam/aidial/cfg/domain/mapper/TimeMapperTest.java
+++ src/test/java/com/epam/aidial/cfg/domain/mapper/TimeMapperTest.java
@@ -82,46 +82,5 @@ class TimeMapperTest {
                 Arguments.of(24L, 86400000L)        // 24 hours
         );
     }
-
-    // Bidirectional consistency tests
-    @Test
-    void testBidirectionalConsistency_HoursToMsToHours() {
-        // given
-        Long originalHours = 5L;
-
-        // when
-        Long milliseconds = mapper.hoursToMs(originalHours);
-        Double convertedBackHours = mapper.msToHours(milliseconds);
-
-        // then
-        Assertions.assertThat(convertedBackHours).isEqualTo(originalHours.doubleValue());
-    }
-
-    @Test
-    void testBidirectionalConsistency_MsToHoursToMs() {
-        // given
-        Long originalMs = MS_IN_HOUR * 3; // 3 hours in milliseconds
-
-        // when
-        Double hours = mapper.msToHours(originalMs);
-        Long convertedBackMs = mapper.hoursToMs(hours.longValue());
-
-        // then
-        Assertions.assertThat(convertedBackMs).isEqualTo(originalMs);
-    }
-
-    @Test
-    void testTruncationBehavior_CompareWithRegularConversion() {
-        // given
-        Long milliseconds = 5400000L; // 1.5 hours
-
-        // when
-        Double regularHours = mapper.msToHours(milliseconds);
-        Long truncatedHours = mapper.msToHoursWithTruncation(milliseconds);
-
-        // then
-        Assertions.assertThat(regularHours).isEqualTo(1.5);
-        Assertions.assertThat(truncatedHours).isEqualTo(1L);
-    }
 }
 
