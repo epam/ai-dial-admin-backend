ff61abc2cfa18a8972932a85eb09f7d27b2966d8
Kiryl_Kurnosenka <Kiryl_Kurnosenka@epam.com>
2025-10-29 19:54:21 +0300

    fix: exclude unlimited cost limits from core export

--- src/main/java/com/epam/aidial/core/config/CoreCostLimit.java
+++ src/main/java/com/epam/aidial/core/config/CoreCostLimit.java
@@ -1,13 +1,19 @@
 package com.epam.aidial.core.config;
 
+import com.epam.aidial.core.config.databind.OmitBigDecimalValueOfLongMaxValueFilter;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.math.BigDecimal;
 
 @Data
 public class CoreCostLimit {
+    @JsonInclude(value = JsonInclude.Include.CUSTOM, valueFilter = OmitBigDecimalValueOfLongMaxValueFilter.class)
     private BigDecimal minute = BigDecimal.valueOf(Long.MAX_VALUE);
+    @JsonInclude(value = JsonInclude.Include.CUSTOM, valueFilter = OmitBigDecimalValueOfLongMaxValueFilter.class)
     private BigDecimal day = BigDecimal.valueOf(Long.MAX_VALUE);
+    @JsonInclude(value = JsonInclude.Include.CUSTOM, valueFilter = OmitBigDecimalValueOfLongMaxValueFilter.class)
     private BigDecimal week = BigDecimal.valueOf(Long.MAX_VALUE);
+    @JsonInclude(value = JsonInclude.Include.CUSTOM, valueFilter = OmitBigDecimalValueOfLongMaxValueFilter.class)
     private BigDecimal month = BigDecimal.valueOf(Long.MAX_VALUE);
 }
