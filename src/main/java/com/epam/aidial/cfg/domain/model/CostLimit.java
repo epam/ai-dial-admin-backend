3ce8e932f1b75e21510e723a82f120138054e41d
Kiryl Kurnosenka <kiryl_kurnosenka@epam.com>
2025-10-30 12:26:57 +0300

    fix: exclude unlimited cost limits from core export (#378)

--- src/main/java/com/epam/aidial/cfg/domain/model/CostLimit.java
+++ src/main/java/com/epam/aidial/cfg/domain/model/CostLimit.java
@@ -10,4 +10,11 @@ public class CostLimit {
     private BigDecimal day = BigDecimal.valueOf(Long.MAX_VALUE);
     private BigDecimal week = BigDecimal.valueOf(Long.MAX_VALUE);
     private BigDecimal month = BigDecimal.valueOf(Long.MAX_VALUE);
+
+    public boolean isUnlimited() {
+        return BigDecimal.valueOf(Long.MAX_VALUE).equals(minute)
+                && BigDecimal.valueOf(Long.MAX_VALUE).equals(day)
+                && BigDecimal.valueOf(Long.MAX_VALUE).equals(week)
+                && BigDecimal.valueOf(Long.MAX_VALUE).equals(month);
+    }
 }
