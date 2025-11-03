40fa77fdd7ae28cb9f7cf7d0440952dc49e02c2e
Kiryl_Kurnosenka <Kiryl_Kurnosenka@epam.com>
2025-11-04 02:26:49 +0300

    fix: fix test

--- src/test/java/com/epam/aidial/cfg/service/impl/storage/ConfigSplitterTest.java
+++ src/test/java/com/epam/aidial/cfg/service/impl/storage/ConfigSplitterTest.java
@@ -39,7 +39,7 @@ class ConfigSplitterTest {
         for (int i = 0; i < 5; i++) {
             configBody.getKeys().put("key" + i, new CoreKey());
         }
-        List<ConfigPart> secretConfigs = splitter.splitConfig(configBody, this::encode, 150, 5);
+        List<ConfigPart> secretConfigs = splitter.splitConfig(configBody, this::encode, 130, 5);
         Assertions.assertEquals(2, secretConfigs.size());
         Assertions.assertEquals(Set.of("key0", "key1", "key2"), secretConfigs.get(0).config().getKeys().keySet());
         Assertions.assertEquals(Set.of("key3", "key4"), secretConfigs.get(1).config().getKeys().keySet());
