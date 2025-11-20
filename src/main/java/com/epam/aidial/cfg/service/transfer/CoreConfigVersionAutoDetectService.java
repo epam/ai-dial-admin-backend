237d345349918c79241f65e0129e10e5d6d6ffdd
Kiryl_Kurnosenka <Kiryl_Kurnosenka@epam.com>
2025-11-20 18:39:45 +0300

    fix!: use core target version when core version auto-detection fails
    BREAKING CHANGE: CORE_CONFIG_VERSION env var becomes required when ENABLE_CORE_CONFIG_VERSION_AUTO_DETECT = false

--- src/main/java/com/epam/aidial/cfg/service/transfer/CoreConfigVersionAutoDetectService.java
+++ src/main/java/com/epam/aidial/cfg/service/transfer/CoreConfigVersionAutoDetectService.java
@@ -7,6 +7,7 @@ import com.epam.aidial.cfg.utils.CoreConfigVersionNormalizer;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
@@ -40,6 +41,9 @@ public class CoreConfigVersionAutoDetectService {
     public String getVersion() {
         if (!coreConfigVersionProperties.isAutoDetectEnabled()) {
             String target = coreConfigVersionProperties.getTarget();
+            if (StringUtils.isBlank(target)) {
+                throw new IllegalStateException("Core target version is undefined");
+            }
             log.debug("Core version auto-detection is disabled. Using target version: {}", target);
             return target;
         }
@@ -62,8 +66,21 @@ public class CoreConfigVersionAutoDetectService {
 
     private String getVersionFromCore() {
         log.debug("Attempting to get version from Core");
-        String version = CoreConfigVersionNormalizer.normalizeCoreVersion(coreConfigClient.getVersion());
-        log.info("Successfully retrieved Core version: {}", version);
+        String version;
+        try {
+            version = CoreConfigVersionNormalizer.normalizeCoreVersion(coreConfigClient.getVersion());
+            log.info("Successfully retrieved Core version: {}", version);
+        } catch (Exception e) {
+            log.info("Unable to retrieve Core version. Will try to use target version", e);
+            String target = coreConfigVersionProperties.getTarget();
+
+            if (StringUtils.isBlank(target)) {
+                throw new IllegalStateException("Core target version is undefined");
+            }
+
+            version = target;
+        }
+
         return version;
     }
 }
