84158cfe31af3866af1be0efa373a3a6d6d9c9d0
Kiryl Kurnosenka <kiryl_kurnosenka@epam.com>
2025-11-21 12:37:52 +0300

    fix!: use core target version when core version auto-detection fails (#437)

--- src/test/java/com/epam/aidial/cfg/service/transfer/CoreConfigVersionAutoDetectServiceTest.java
+++ src/test/java/com/epam/aidial/cfg/service/transfer/CoreConfigVersionAutoDetectServiceTest.java
@@ -2,9 +2,12 @@ package com.epam.aidial.cfg.service.transfer;
 
 import com.epam.aidial.cfg.client.AnonymousCoreConfigClient;
 import com.epam.aidial.cfg.configuration.CoreConfigVersionProperties;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -50,6 +53,26 @@ class CoreConfigVersionAutoDetectServiceTest {
         verifyNoMoreInteractions(coreConfigClient);
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {"null", "''", "' '"}, nullValues = "null")
+    void getVersion_autoDetectDisabled_throwsExceptionIfTargetVersionIsBlank(String version) {
+        // Given
+        when(properties.isAutoDetectEnabled()).thenReturn(false);
+        when(properties.getTarget()).thenReturn(version);
+
+        // When
+        IllegalStateException exception = Assertions.assertThrows(
+                IllegalStateException.class,
+                () -> service.getVersion()
+        );
+
+        // Then
+        assertEquals("Core target version is undefined", exception.getMessage());
+        verify(properties).isAutoDetectEnabled();
+        verify(properties).getTarget();
+        verifyNoMoreInteractions(coreConfigClient);
+    }
+
     @Test
     void getVersion_successfulApiCall_returnsCoreVersion() {
         // Given
@@ -76,6 +99,42 @@ class CoreConfigVersionAutoDetectServiceTest {
 
         // Then
         assertEquals(CORE_VERSION, result);
+        verify(properties).isAutoDetectEnabled();
+        verify(coreConfigClient).getVersion();
+    }
+
+    @Test
+    void getVersion_unsuccessfulApiCall_returnsTargetVersion() {
+        // Given
+        when(properties.isAutoDetectEnabled()).thenReturn(true);
+        when(properties.getTarget()).thenReturn(TARGET_VERSION);
+        when(coreConfigClient.getVersion()).thenThrow(new RuntimeException());
+
+        // When
+        String result = service.getVersion();
+
+        // Then
+        assertEquals(TARGET_VERSION, result);
+        verify(properties).isAutoDetectEnabled();
+        verify(coreConfigClient).getVersion();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"null", "''", "' '"}, nullValues = "null")
+    void getVersion_unsuccessfulApiCall_throwsExceptionIfTargetVersionIsBlank(String version) {
+        // Given
+        when(properties.isAutoDetectEnabled()).thenReturn(true);
+        when(properties.getTarget()).thenReturn(version);
+        when(coreConfigClient.getVersion()).thenThrow(new RuntimeException());
+
+        // When
+        IllegalStateException exception = Assertions.assertThrows(
+                IllegalStateException.class,
+                () -> service.getVersion()
+        );
+
+        // Then
+        assertEquals("Core target version is undefined", exception.getMessage());
         verify(properties).isAutoDetectEnabled();
         verify(coreConfigClient).getVersion();
     }
