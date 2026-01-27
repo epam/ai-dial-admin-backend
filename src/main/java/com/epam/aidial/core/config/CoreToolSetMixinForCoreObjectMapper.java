14e6ec8b8969f9a34f9502ca3d0fbd4561fcd345
Kiryl_Kurnosenka <Kiryl_Kurnosenka@epam.com>
2026-01-27 14:53:42 +0300

    fix: adjust export of toolset secret part

---
+++ src/main/java/com/epam/aidial/core/config/CoreToolSetMixinForCoreObjectMapper.java
@@ -0,0 +1,7 @@
+package com.epam.aidial.core.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public abstract class CoreToolSetMixinForCoreObjectMapper extends CoreSecuredResource {
+}
