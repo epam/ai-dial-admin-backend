02f72dd8a1d8d54773312f452c15af28cc217d92
Katerina_Charakhovich <katerinacharakhovich@gmail.com>
2026-01-27 12:28:33 +0100

    initial commit

--- src/main/java/com/epam/aidial/cfg/service/config/reload/CoreConfigReloadScheduler.java
+++ src/main/java/com/epam/aidial/cfg/service/config/reload/CoreConfigReloadScheduler.java
@@ -2,6 +2,7 @@ package com.epam.aidial.cfg.service.config.reload;
 
 import com.epam.aidial.cfg.client.BackendTokenAuthenticatedCoreClient;
 import com.epam.aidial.cfg.configuration.logging.LogExecution;
+import com.epam.aidial.cfg.exception.CoreConfigReloadException;
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,9 +28,10 @@ public class CoreConfigReloadScheduler {
             coreConfigReloadCache.put(config);
             errorHandler.setLastErrorMessage(null);
         } catch (Exception e) {
-            log.error("Error during config reload", e);
+            log.error("Failed to reload configuration in DIAL Core", e);
             String lastErrorMessage = e.getMessage() == null ? "An unknown error occurred during config reload" : e.getMessage();
             errorHandler.setLastErrorMessage(lastErrorMessage);
+            throw new CoreConfigReloadException("Failed to reload configuration in DIAL Core");
         }
     }
 
