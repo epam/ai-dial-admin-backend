48522257722ccc97270be40b9d3f8ac49d370c33
Katerina_Charakhovich <katerinacharakhovich@gmail.com>
2026-01-30 13:04:10 +0100

    addressed comments

--- src/main/resources/db/migration/POSTGRES/V1.89__AddAllowedIpAddressRangesFieldToKeyTables.sql
+++ src/main/resources/db/migration/POSTGRES/V1.89__AddAllowedIpAddressRangesFieldToKeyTables.sql
@@ -1,3 +1,3 @@
 -- add new field allowed_ip_address_ranges to key_entity, key_entity_aud tables
-alter table key_entity add column allowed_ip_address_ranges text array;
-alter table key_entity_aud add column allowed_ip_address_ranges text array;
+alter table key_entity add column allowed_ip_address_ranges text;
+alter table key_entity_aud add column allowed_ip_address_ranges text;
