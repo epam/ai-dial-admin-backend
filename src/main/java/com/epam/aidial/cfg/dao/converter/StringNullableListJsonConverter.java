71ede16a5c04ecdade301dd99b180a99342cb4cd
Kiryl_Kurnosenka <Kiryl_Kurnosenka@epam.com>
2026-02-02 13:09:53 +0300

    fix: read allowedIpAddressRanges as null instead of empty list

---
+++ src/main/java/com/epam/aidial/cfg/dao/converter/StringNullableListJsonConverter.java
@@ -0,0 +1,40 @@
+package com.epam.aidial.cfg.dao.converter;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.AttributeConverter;
+import jakarta.persistence.Converter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Converter
+@Slf4j
+public class StringNullableListJsonConverter implements AttributeConverter<List<String>, String> {
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public String convertToDatabaseColumn(List<String> attribute) {
+        try {
+            return attribute == null ? null : mapper.writeValueAsString(attribute);
+        } catch (Exception e) {
+            log.debug("Failed to convert list to data. Attribute value: {}", attribute, e);
+            throw new IllegalStateException("Failed to convert list to data", e);
+        }
+    }
+
+    @Override
+    public List<String> convertToEntityAttribute(String data) {
+        try {
+            return data == null
+                    ? null
+                    : mapper.readValue(data, new TypeReference<>() {
+            });
+        } catch (Exception e) {
+            log.debug("Failed to convert data to list. Raw attribute value: {}", data, e);
+            throw new IllegalStateException("Failed to convert data to list", e);
+
+        }
+    }
+}
