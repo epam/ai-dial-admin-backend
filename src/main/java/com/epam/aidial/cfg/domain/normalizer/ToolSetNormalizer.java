bfb5a15573b839d5dafe0d7cee17149e18c9b213
Kiryl Kurnosenka <kiryl_kurnosenka@epam.com>
2026-03-02 19:37:30 +0300

    fix: support PKCE data generation during toolset creation/update (#721)

--- src/main/java/com/epam/aidial/cfg/domain/normalizer/ToolSetNormalizer.java
+++ src/main/java/com/epam/aidial/cfg/domain/normalizer/ToolSetNormalizer.java
@@ -1,14 +1,24 @@
 package com.epam.aidial.cfg.domain.normalizer;
 
+import com.epam.aidial.cfg.domain.model.SecuredResource;
 import com.epam.aidial.cfg.domain.model.ToolSet;
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class ToolSetNormalizer {
+
+    private final ResourceAuthSettingsNormalizer resourceAuthSettingsNormalizer;
 
     public void normalize(ToolSet toolSet) {
         setEndpointToNullIfBlank(toolSet);
+
+        SecuredResource deployment = toolSet.getDeployment();
+        if (deployment != null) {
+            resourceAuthSettingsNormalizer.normalize(deployment.getAuthSettings());
+        }
     }
 
     private void setEndpointToNullIfBlank(ToolSet toolSet) {
