559ec04619157b552606b8ca0dbeb10299f7d2d2
Kiryl_Kurnosenka <Kiryl_Kurnosenka@epam.com>
2026-03-17 00:16:24 +0300

    fix: recalculate application validity state when associated app schema is changed

---
+++ src/main/java/com/epam/aidial/cfg/dao/listener/validitystate/ApplicationTypeSchemaEntityListener.java
@@ -0,0 +1,21 @@
+package com.epam.aidial.cfg.dao.listener.validitystate;
+
+import com.epam.aidial.cfg.configuration.logging.LogExecution;
+import com.epam.aidial.cfg.dao.model.ApplicationTypeSchemaEntity;
+import jakarta.persistence.PreUpdate;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@LogExecution
+public class ApplicationTypeSchemaEntityListener {
+
+    @PreUpdate
+    public void preUpdate(ApplicationTypeSchemaEntity entity) {
+        handleEntity(entity);
+    }
+
+    private void handleEntity(ApplicationTypeSchemaEntity entity) {
+        // will trigger preUpdate for application where actual validity state will be calculated and set
+        entity.getApplications().forEach(app -> app.setValidityState(null));
+    }
+}
