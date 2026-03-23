6f1441bb868b7af6384a05d3545c2eb2dadf796c
Kiryl_Kurnosenka <Kiryl_Kurnosenka@epam.com>
2026-03-23 22:19:15 +0300

    fix: fix postgres script

--- src/main/resources/db/migration/POSTGRES/V1.94__IncreaseResourceIdColumnLengthInAuditActivityEntityTable.sql
+++ src/main/resources/db/migration/POSTGRES/V1.94__IncreaseResourceIdColumnLengthInAuditActivityEntityTable.sql
@@ -2,4 +2,4 @@
 -- RoleLimitId(deploymentName={deploymentName}, roleName={roleName})
 -- since {deploymentName} and {roleName} are varchar(255) then length of role limit id is equal to
 -- 255 + 255 + "RoleLimitId(deploymentName=, roleName=)".length() = 255 + 255 + 39 = 549
-alter table if exists audit_activity_entity alter column resource_id varchar(549);
+alter table if exists audit_activity_entity alter column resource_id type varchar(549);
