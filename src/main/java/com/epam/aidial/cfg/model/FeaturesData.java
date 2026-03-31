38adf802ce887fd0e4be38ae886c84b6434d92e9
Kiryl Kurnosenka <kiryl_kurnosenka@epam.com>
2026-03-31 14:18:49 +0300

    fix: provide auth status for toolset entity (#811)

---
+++ src/main/java/com/epam/aidial/cfg/model/FeaturesData.java
@@ -0,0 +1,31 @@
+package com.epam.aidial.cfg.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FeaturesData {
+
+    private boolean rate;
+    private boolean tokenize;
+    private boolean truncatePrompt;
+    private boolean configuration;
+    private boolean systemPrompt;
+    private boolean tools;
+    private boolean seed;
+    private boolean urlAttachments;
+    private boolean folderAttachments;
+    private boolean allowResume;
+    private boolean accessibleByPerRequestKey;
+    private boolean contentParts;
+    private boolean temperature;
+    private boolean cache;
+    private boolean autoCaching;
+    private boolean parallelToolCalls;
+    private boolean assistantAttachmentsInRequest;
+}
