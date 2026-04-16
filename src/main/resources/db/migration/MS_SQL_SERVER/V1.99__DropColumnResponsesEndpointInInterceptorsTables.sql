463cd2287bd6b2efe08af29dbd805544b5782327
Katerina_Charakhovich <katerinacharakhovich@gmail.com>
2026-04-16 19:35:26 +0200

    check sql script

--- src/main/resources/db/migration/MS_SQL_SERVER/V1.99__DropColumnResponsesEndpointInInterceptorsTables.sql
+++ src/main/resources/db/migration/MS_SQL_SERVER/V1.99__DropColumnResponsesEndpointInInterceptorsTables.sql
@@ -1,2 +1,2 @@
-alter table if exists interceptor_entity drop column responses_endpoint;
-alter table if exists interceptor_entity_aud drop column responses_endpoint;
+alter table interceptor_entity drop column responses_endpoint;
+alter table interceptor_entity_aud drop column responses_endpoint;
