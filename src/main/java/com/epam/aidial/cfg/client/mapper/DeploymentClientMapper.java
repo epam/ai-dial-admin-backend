c23b7febfdb2761c58cee88a0b46c5954112c1b9
Katerina_Charakhovich <katerinacharakhovich@gmail.com>
2026-05-20 11:04:57 +0200

    addressed comments

--- src/main/java/com/epam/aidial/cfg/client/mapper/DeploymentClientMapper.java
+++ src/main/java/com/epam/aidial/cfg/client/mapper/DeploymentClientMapper.java
@@ -8,30 +8,24 @@ import com.epam.aidial.cfg.model.ApplicationData;
 import com.epam.aidial.cfg.model.DeploymentData;
 import com.epam.aidial.cfg.model.ModelData;
 import com.epam.aidial.cfg.model.ToolSetData;
-import lombok.extern.slf4j.Slf4j;
 import org.mapstruct.Mapper;
 import org.mapstruct.SubclassMapping;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
-@Mapper(componentModel = "spring")
-@Slf4j
-public abstract class DeploymentClientMapper {
+@Mapper(componentModel = "spring", uses = RouteMapper.class)
+public interface DeploymentClientMapper {
 
-    @Autowired
-    protected RouteMapper routeMapper;
-
-    public abstract List<DeploymentData> toDeploymentDataList(List<DeploymentDataDto> dtos);
+    List<DeploymentData> toDeploymentDataList(List<DeploymentDataDto> dtos);
 
     @SubclassMapping(source = ModelDataDto.class, target = ModelData.class)
     @SubclassMapping(source = ApplicationDataDto.class, target = ApplicationData.class)
     @SubclassMapping(source = ToolSetDataDto.class, target = ToolSetData.class)
-    public abstract DeploymentData toDeploymentData(DeploymentDataDto dto);
+    DeploymentData toDeploymentData(DeploymentDataDto dto);
 
-    public abstract ApplicationData toApplicationData(ApplicationDataDto dto);
+    ApplicationData toApplicationData(ApplicationDataDto dto);
 
-    public abstract ModelData toModelData(ModelDataDto dto);
+    ModelData toModelData(ModelDataDto dto);
 
-    public abstract ToolSetData toToolSetData(ToolSetDataDto dto);
+    ToolSetData toToolSetData(ToolSetDataDto dto);
 }
