a06809091350970c5e58dfe075cd521d156f8c19
Katerina_Charakhovich <katerinacharakhovich@gmail.com>
2026-07-16 15:23:42 +0200

    addressed comments

--- src/main/resources/db/migration/MS_SQL_SERVER/V1.115__AddAppIdentityAndAllowUserExternalServicesToApplicationAndIntroToApplicationModelToolSetTables.sql
+++ src/main/resources/db/migration/MS_SQL_SERVER/V1.115__AddAppIdentityAndAllowUserExternalServicesToApplicationAndIntroToApplicationModelToolSetTables.sql
@@ -1,25 +1,25 @@
 -- add app_identity, allow_user_external_services fields to application tables
-alter table application_entity add app_identity nvarchar(max);
+alter table application_entity add app_identity nvarchar(255);
 alter table application_entity add allow_user_external_services bit;
 go
 update application_entity set allow_user_external_services = 0 where allow_user_external_services is null;
 
-alter table application_entity_aud add app_identity nvarchar(max);
+alter table application_entity_aud add app_identity nvarchar(255);
 alter table application_entity_aud add allow_user_external_services bit;
 go
 update application_entity_aud set allow_user_external_services = 0 where allow_user_external_services is null and revtype != 2;
 
 -- add intro field to application tables
-alter table application_entity add intro nvarchar(max);
-alter table application_entity_aud add intro nvarchar(max);
+alter table application_entity add intro nvarchar(255);
+alter table application_entity_aud add intro nvarchar(255);
 
 -- add intro field to model tables
-alter table model_entity add intro nvarchar(max);
-alter table model_entity_aud add intro nvarchar(max);
+alter table model_entity add intro nvarchar(255);
+alter table model_entity_aud add intro nvarchar(255);
 
 -- add intro field to tool_set tables
-alter table tool_set_entity add intro nvarchar(max);
-alter table tool_set_entity_aud add intro nvarchar(max);
+alter table tool_set_entity add intro nvarchar(255);
+alter table tool_set_entity_aud add intro nvarchar(255);
 
 -- add dynamically_registered field to deployment_entity tables
 alter table deployment_entity add dynamically_registered bit;
