bb0199a6d432221a1a8e5a0922c1376920094b5e
Katerina-Charakhovich <78045313+Katerina-Charakhovich@users.noreply.github.com>
2026-09-18 12:46:02 +0200

    feat: add tree structure pricing  (#1155)

---
+++ src/main/java/com/epam/aidial/cfg/domain/model/PricingRate.java
@@ -0,0 +1,25 @@
+package com.epam.aidial.cfg.domain.model;
+
+import com.epam.aidial.cfg.domain.model.databind.PricingRateDeserializer;
+import com.epam.aidial.cfg.domain.model.databind.PricingRateSerializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.Data;
+
+@Data
+@JsonDeserialize(using = PricingRateDeserializer.class)
+@JsonSerialize(using = PricingRateSerializer.class)
+public class PricingRate {
+
+    // leaf shape
+    private String rate;
+
+    // node shape; ifTrue/ifFalse omitted -> falls back to the prompt rate
+    private PricingRateCondition test;
+    private PricingRate ifTrue;
+    private PricingRate ifFalse;
+
+    public boolean isLeaf() {
+        return rate != null;
+    }
+}
